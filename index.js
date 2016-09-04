9112f71fdf979299ea5597ce42a5f08e9f83c3a2
Mikel Severson <mkseve@gmail.com>
2016-09-04 18:41:49 -0500

    feat: Adding cron jobs

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
 var Particle = require('particle-api-js'),
-    firebase = require('firebase');
-    env      = require('node-env-file')
+    firebase = require('firebase'),
+    CronJob  = require('cron').CronJob,
+    _        = require('lodash');
+
+require('node-env-file')('./credentials.env');
 
 firebase.initializeApp({
     databaseURL: process.env.DATABASE_URL,
     serviceAccount: "./serviceAccountCredentials.json"
-})
+});
 
 var particle = new Particle();
-env('./credentials.env');
 var db = firebase.database();
 
-particle.login({username: process.env.USERNAME, password: process.env.PASSWORD}).then(
-  function(data){
-        console.log(`Successfully logged into particle`);
-        particle.getEventStream({ deviceId: process.env.DEVICE_ID, auth: data.body.access_token }).then(function(stream) {
-        stream.on('event', function(data) {
-            switch(data.name) {
-                case 'humidity': 
-                    db.ref('Humidity').set(data.data + "%")
-                    break;
-                case 'tempF': 
-                    db.ref('Temperature').set(data.data + "°")
-                    break;
-                case 'lightValue': 
-                    if(data.data > 100) {
-                        db.ref('Lights').set(true)
-                    } else {
-                        db.ref('Lights').set(false)
-                    }
-                    break;
-            }
-        });
+particleLogin();
+
+function particleLogin() {
+  particle.login({
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  }).then(data => {
+      console.log(`Successfully logged into particle`);
+      particle.getEventStream({
+        deviceId: process.env.DEVICE_ID,
+        auth: data.body.access_token
+      }).then(stream => {
+        stream.on('event', newData);
+      });
+    }, error => {
+      console.log('Failed to log into Particle: ', error);
     });
-  },
-  function(err) {
-    console.log('API call completed on promise fail: ', err);
+}
+
+const newData = data => {
+  const timestamp = Date.now();
+  switch(data.name) {
+      case 'humidity':
+          const humidity = data.data + "%"
+          db.ref('current/humidity').set(humidity)
+          db.ref('Humidity/' + timestamp).set(humidity)
+          break;
+      case 'tempF':
+          const temperature = data.data + "°"
+          db.ref('current/temperature').set(temperature)
+          db.ref('Temperature/' + timestamp).set(temperature)
+          break;
+      case 'lightValue':
+          db.ref('Lights/' + timestamp).set(data.data);
+          break;
   }
-);
+}
+
+new CronJob('* * * * * *', () => {
+  const refs = [db.ref('Humidity'), db.ref('Temperature'), db.ref('Lights')];
+  _.forEach(refs, ref => {
+    const dataArray = [];
+    ref.once("value", data => {
+      data.forEach(data => {
+        dataArray.push(parseInt(data.val(), 10));
+      });
+      ref.child('mean').set(_.mean(dataArray));
+    });
+  })
+}, null, true, 'America/Los_Angeles');
