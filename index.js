21258be4658934c0c4c46080e6884316ddc581d2
Mikel Severson <mkseve@gmail.com>
2016-09-05 19:18:32 -0500

    feat(averaging): experimenting with cron job for averaging Currently messy, but a proof of concept

--- index.js
+++ index.js
@@ -38,28 +38,38 @@ const newData = data => {
       case 'humidity':
           const humidity = data.data + "%"
           db.ref('current/humidity').set(humidity)
-          db.ref('Humidity/' + timestamp).set(humidity)
+          db.ref('Humidity/gathering/' + timestamp).set(humidity)
           break;
       case 'tempF':
           const temperature = data.data + "°"
           db.ref('current/temperature').set(temperature)
-          db.ref('Temperature/' + timestamp).set(temperature)
+          db.ref('Temperature/gathering/' + timestamp).set(temperature)
           break;
       case 'lightValue':
-          db.ref('Lights/' + timestamp).set(data.data);
+          db.ref('Lights/gathering/' + timestamp).set(data.data);
           break;
   }
 }
 
-new CronJob('* * * * * *', () => {
+const averageCron = new CronJob('0 0 * * * *', () => {
   const refs = [db.ref('Humidity'), db.ref('Temperature'), db.ref('Lights')];
   _.forEach(refs, ref => {
     const dataArray = [];
-    ref.once("value", data => {
+    ref.child('gathering').once('value', data => {
       data.forEach(data => {
-        dataArray.push(parseInt(data.val(), 10));
+        const dataInt = parseInt(data.val(), 10);
+        if(Number.isInteger(dataInt)) {
+          dataArray.push(dataInt);
+        }
       });
-      ref.child('mean').set(_.mean(dataArray));
+      if(dataArray.length > 1) {
+        const mean = _.mean(dataArray);
+        ref.child('gathering').set({});
+        const d = new Date();
+        d.setMinutes(d.getMinutes() + 30);
+        d.setMinutes(0);
+        ref.child('mean').child(d.getTime()).set(mean);
+      }
     });
   })
 }, null, true, 'America/Los_Angeles');
