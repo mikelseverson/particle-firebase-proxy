c1057ae2fd22727068e16039b4055d83fe3ce15d
Mikel Severson <mkseve@gmail.com>
2018-03-17 14:54:42 -0500

    feat: small refactor

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const Particle = require('particle-api-js'),
-      firebase = require('firebase'),
-      CronJob  = require('cron').CronJob,
-      _        = require('lodash');
-      env      = require('node-env-file')('./credentials.env');
+  firebase = require('firebase'),
+  CronJob = require('cron').CronJob,
+  _ = require('lodash'),
+  env = require('node-env-file')('./credentials.env');
 
 firebase.initializeApp({
-    databaseURL: process.env.DATABASE_URL,
-    serviceAccount: process.env.SERVICE_ACCOUNT
+  databaseURL: process.env.DATABASE_URL,
+  serviceAccount: process.env.SERVICE_ACCOUNT
 });
 
 const particle = new Particle();
 const db = firebase.database();
 
-
 particle.login({
   username: process.env.USERNAME,
   password: process.env.PASSWORD
 }).then(data => {
-    console.log(`Successfully logged into particle`);
-    particleStream(data.body.access_token);
-  }, error => {
-    console.log('Failed to log into Particle: ', error);
-  });
+  console.log(`Successfully logged into particle`);
+  particleStream(data.body.access_token);
+}, error => {
+  console.log('Failed to log into Particle: ', error);
+});
 
 const particleStream = auth => {
   particle.getEventStream({
@@ -36,56 +35,54 @@ const particleStream = auth => {
 
 const newData = data => {
   const timestamp = Date.now();
-
-  switch(data.name) {
-      case 'PH':
-          const PH = Number(data.data)
-          db.ref('current/ph').set(PH)
-          db.ref(`PH/gathering/${timestamp}`).set(PH);
-          break;
-      case 'humidity':
-          const humidity = data.data + "%"
-          db.ref('current/humidity').set(humidity)
-          db.ref(`Humidity/gathering/${timestamp}`).set(data.data)
-          break;
-      case 'tempF':
-          const temperature = data.data + "°"
-          db.ref('current/temperature').set(temperature)
-          db.ref(`Temperature/gathering/${timestamp}`).set(data.data)
-          break;
-      case 'lightValue':
-          db.ref('current/light').set(data.data)
-          db.ref(`Lights/gathering/${timestamp}`).set(data.data);
-          break;
+  switch (data.name) {
+    case 'PH':
+      const PH = Number(data.data)
+      db.ref('current/ph').set(PH)
+      db.ref(`PH/gathering/${Date.now()}`).set(PH);
+      break;
+    case 'humidity':
+      const humidity = data.data + "%"
+      db.ref('current/humidity').set(humidity)
+      db.ref(`Humidity/gathering/${Date.now()}`).set(data.data)
+      break;
+    case 'tempF':
+      const temperature = data.data + "°"
+      db.ref('current/temperature').set(temperature)
+      db.ref(`Temperature/gathering/${Date.now()}`).set(data.data)
+      break;
+    case 'lightValue':
+      db.ref('current/light').set(data.data)
+      db.ref(`Lights/gathering/${Date.now()}`).set(data.data);
+      break;
   }
 }
 
 const propagateData = (data, ref) => {
-  const timestamp = Date.now();
   let meanData,
-      dataArray = [];
+    dataArray = [];
 
   _.forOwn(data, dataSnapshot => {
     dataArray.push(dataSnapshot.value);
   });
 
-  meanData = _.mean(dataArray);
-
   ref.child('gathering').set({});
-  ref.child('mean').child(timestamp).set(meanData);
+  ref.child('mean').child(Date.now()).set(_.mean(dataArray));
 }
 
+const initializeFifteenMinCron = () =>
+  new CronJob('* */15 * * * *', () => cronHandler);
 
-const fiteenMinCron = new CronJob('* */15 * * * *', () => {
-  const refs = [db.ref('Humidity'),
-                db.ref('Temperature'),
-                db.ref('Lights'),
-                db.ref('PH')];
-  _.forEach(refs, ref => {
+const cronHandler = () => [
+    db.ref('Humidity'),
+    db.ref('Temperature'),
+    db.ref('Lights'),
+    db.ref('PH'),
+  ].map(ref =>
     ref.child('gathering').once('value')
-     .then(dataSnap => {
-      const data       = dataSnap.val(),
-            parsedData = [];
+    .then(dataSnap => {
+      const data = dataSnap.val(),
+        parsedData = [];
 
       _.forOwn(data, (data, timestamp) => {
         parsedData.push({
@@ -93,9 +90,7 @@ const fiteenMinCron = new CronJob('* */15 * * * *', () => {
           timestamp: timestamp
         });
       });
-      if(parsedData.length >= 1) {
+      if (parsedData.length >= 1) {
         propagateData(parsedData, ref);
       }
-    });
-  })
-}, null, true, 'America/Los_Angeles');
+    }), null, true, 'America/Los_Angeles');
